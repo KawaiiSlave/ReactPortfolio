62667aecc148d374f1eb8e9d84003091555e8833
Tyler <tgandle@gmail.com>
2020-04-22 21:22:39 -0500

    added bootstrap navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Navbar from "./components/Navbar"
-
+import Nav from './components/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      <Nav/>
       
     </div>
   );
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,42 +1,15 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import React from "react";
+import Navbar from 'react-bootstrap/Navbar'
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
-
-export default function Navbar() {
-  const classes = useStyles();
-
+function Nav() {
   return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Tyler Goodman
-          </Typography>
-          <Button color="inherit">Resume</Button>
-          <Button color="inherit">Github</Button>
-          <Button color="inherit">LinkedIn</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
+  <Navbar bg="primary" variant="dark">
+    <Navbar.Brand>Tyler Goodman</Navbar.Brand>
+
+
+  </Navbar>
+ 
   );
 }
+
+export default Nav;
